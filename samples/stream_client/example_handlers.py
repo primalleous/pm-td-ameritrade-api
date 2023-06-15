64e9c1f10b34e4565fca0a26ad0b4fff6792c638
primalmachina <primalmachinaz@gmail.com>
2023-06-15 06:15:08 -0500

    SymbolDataUpdater example fix websocket bug

--- samples/stream_client/example_handlers.py
+++ samples/stream_client/example_handlers.py
@@ -71,7 +71,9 @@ class SymbolDataUpdater(BaseDataMessageHandler):
 
     async def subscribe_websocket(self, websocket, symbol):
         if symbol in self.latest_data_by_symbol:
-            await websocket.send_json([self.latest_data_by_symbol[symbol].dict()])
+            await websocket.send_json(
+                [x.dict() for x in self.latest_data_by_symbol[symbol]]
+            )
         if websocket not in self.subscribers:
             self.subscribers[websocket] = set()
         self.subscribers[websocket].add(symbol)
